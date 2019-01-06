5f2d529c0d2f818ac5e89a18e9b19a2764f2de1a
Codingmace <Codingmace@users.noreply.github.com>
2019-01-06 14:23:51 -0600

    Version 1.1
    Cleaned up the viewing and removed Excess stuff

--- src/Mp4toGif2.py
+++ src/Mp4toGif2.py
@@ -2,18 +2,21 @@ import datetime
 import os
 import sys
 from cv2 import imwrite, CAP_PROP_FRAME_COUNT, CAP_PROP_POS_AVI_RATIO, \
-    CAP_PROP_POS_MSEC
+    CAP_PROP_POS_MSEC, CAP_PROP_FRAME_HEIGHT, CAP_PROP_FRAME_WIDTH
 import cv2
 import ffmpy
 import imageio
 import numpy as np
+
+
 import tinify 
 tinify.key = "6QP9ymBx7CRyw6sWB1HFyBQC7Q9Z2jGQ" # Create API Key to compress images
 
-
-
-
-
+# Compressing the video using Tinify
+''' Check as may run into problems overriding a file '''
+def tinyComp(filepath):
+    source = tinify.from_file(filepath)
+    source.to_file(filepath)
 
 
 
@@ -28,25 +31,76 @@ def main():
     # Inputting speed
     print("I need to collect some data about our new file")
     print("If you need any help just enter define and I will print some help")
-    initd = input("Do you want more than one frame per second ('Yes' or 'No'): "
+    initd = input("Do you want more than one frame per second ('Yes' or 'No'): ")
     duration = 0
     if ("y" in initd): # If you want long videos
         print("Ok so that means its going to be long.")
         duration = input("You want a frame every how many seconds? ")
     else: # If you want Speed up videos
-        print("Thia is going to be a compression, Fun. I love these")
+        print("This is going to be a compression, Fun. I love these")
         duration = input("How many frames do you want in a second? ")
-        duration = 1/ duration
+        duration = 1 / duration
+    duration = float(duration)
+    print("Now we must consider the power of the software and Each Videos Properties")
+    ''' Printing the properties of the video file '''
+    framerate = vidcap.get(5)
+    framecount = vidcap.get(7)
+    print("Frame Rate: ", framerate)
+    print("Frame Count: ", framecount)
+    print("Frame Height: ", vidcap.get(CAP_PROP_FRAME_HEIGHT))
+    print("Frame Width: ", vidcap.get(CAP_PROP_FRAME_WIDTH))
+    print("Number of Frames: ", vidcap.get(CAP_PROP_FRAME_COUNT))
+    print("Total Number of Frames: ", framecount * framerate)
+    ''' Creating the output folder if it doesn't exist '''
+    outFold = 'data1' # Outputting folder
+    try:
+        if not os.path.exists(outFold):
+            os.makedirs(outFold)
+    except OSError:
+        print ('Error: Creating directory of data')
+    
+    
+    
     speedRate = input("Input the number of frames you want")
     """ Need to add here  the speed rate checking to see if it is a number"""
     print(type(speedRate))
     speedRate = float(speedRate)
-    
+    ''' The Actual process of breaking up the MP4 '''
+    con = 0 # Counter of the file
+    fileNumb = 0 # The file Number
+    while fileNumb < (con * secTime):
+        if con > 200: # For the longer files cut off
+            print("You trying to break this. I'm cutting you off")
+            break
+        success, image = vidcap.read()
+        if success:
+            cv2.imwrite(outFold+"/frame" + str(fileNumb) + ".jpg", image)  # save frame as JPEG file
+        else: # At the end of the file
+            break
+        con+= 1/ speedRate # Adding time to next frame
+        fileNumb += 1 # Next frame
+    """ Creating the GIF with the obtained Files """
+    images = []
+    for i in range(fileNumb):
+        filename = "data6/frame" + str(i) + ".jpg"
+        images.append(imageio.imread(filename));
+    output_file = 'Gif-%s.gif' % datetime.datetime.now().strftime('%Y-%M-%d-%H-%M-%S')
+    imageio.mimsave(output_file, images, duration=duration)
+    print("Time to clean up")
+    print("Releasing all the lose ends")
+    vidcap.release()
+    print("Destroying all the evidence. MWahhhahha")
+    cv2.destroyAllWindows()
+    print("Welp, there we go. We are all done.")
+    print("Continue with the rest of your day")
 
 
 
 
-    
+
+
+
+'''    
 """
 Splitting video into images
 """
@@ -61,18 +115,9 @@ print("Now it is time for some compression input")
 speedRate = float(input("Input a number: "))
 
 count = 0
-framerate = vidcap.get(5)
-print("framerate:", framerate)
-framecount = vidcap.get(7)
-print("framecount:", framecount)
-vidcap.set(5, 1)
-newframerate = vidcap.get(5)
-print("newframerate:", newframerate)
-print("NUMBER OF FRAMES: ", vidcap.get(CAP_PROP_FRAME_COUNT))
-framenumb = framecount * framerate
+
 
 secTime = 1000  # Miliseconds in a second
-# secTime = 100
 pos = CAP_PROP_POS_AVI_RATIO
 con = 0
 fileNumb = 0  # File number count
@@ -95,10 +140,11 @@ while framenumb > (con * secTime):
 imagee = int(con * speedRate)  # Number of last file using
 print(str(imagee))
 print(fileNumb)
-#    create_gif(imagee ,2)
+'''
 ''' 
 Creating the gif
 '''
+"""
 duration = .1
 images = []
 for i in range(fileNumb):
@@ -128,7 +174,6 @@ Which will produce a folder called data with the images. There will be 2000+ ima
 # Playing video from file:
 
 cap = cv2.VideoCapture('./Buizel (12-3).MP4')
-# ior = imageio.get_reader("./Buizel (12-3).MP4",mode='I')
 
 outFold = 'data'
 try:
@@ -141,18 +186,6 @@ if (True):  # Used to save space
     exit();
 currentFrame = 0
 while(currentFrame < 10):
-    # Capture frame-by-frame
-#    for x in range(30000):
-#        ret, frame = cap.read()
-#    q = cap.get(currentFrame)
-
-#    cap.grab()
-#    for x in range(10000):
-#        ret, frame = cap.read()
-#    cap.get(currentFrame * 10000)
-#        cap.read()
-#    cap.set(1, currentFrame)
-#        print(x)
     ret, frame = cap.read()
     
     # Saves image of the current frame in jpg file
@@ -166,26 +199,4 @@ while(currentFrame < 10):
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
-
-"""
-Turning the mp4 into images
-"""
-"""
-images = []
-filename = 'filename.mp4'
-with imageio.get_writer('/path/to/movie.gif', mode='I') as writer:
-    for filename in images:
-        image = imageio.imread(filename)
-        writer.append_data(image)
-        
-"""
-
-"""
-Converts the Gif into a MP4
-"""
-"""
-startGif = 'input.gif'
-outputFile = 'm.mp4'
-ff = ffmpy.FFmpeg(inputs={startGif:None},outputs={outputFile:None})
-ff.run()
 """
